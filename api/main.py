b77b034bcdcecd06abad890b190774b55101dad9
Nicolas <nicolas.test612@gmail.com>
2024-08-06 18:22:00 -0500

    Mensaje descriptivo sobre los cambios

--- api/main.py
+++ api/main.py
@@ -32,7 +32,22 @@ class Tuya_methods:
 
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("key.json", scope)
+
+key_json = {
+    "type": os.getenv("GOOGLE_SERVICE_ACCOUNT_TYPE"),
+    "project_id": os.getenv("GOOGLE_PROJECT_ID"),
+    "private_key_id": os.getenv("GOOGLE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("GOOGLE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
+    "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+    "auth_uri": os.getenv("GOOGLE_AUTH_URI"),
+    "token_uri": os.getenv("GOOGLE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("GOOGLE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("GOOGLE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("GOOGLE_UNIVERSE_DOMAIN")
+}
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(key_json, scope)
 client = gspread.authorize(creds)
 
 # Abrir la hoja de cálculo
