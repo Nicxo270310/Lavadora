5238ad40278e51026fe58e38cf4e88a1a1cdcbfb
Nicolas <nicolas.test612@gmail.com>
2024-08-07 10:18:28 -0500

    Mensaje descriptivo sobre los cambios

--- api/main.py
+++ api/main.py
@@ -2,11 +2,11 @@ from tuya_iot import TuyaOpenAPI
 from tuya_connector import TuyaOpenAPI
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import schedule
-import time
 from datetime import datetime
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
+import schedule
+import threading
 
 app = Flask(__name__)
 
@@ -17,7 +17,6 @@ DEVICE_ID = '72286240600194c4d67a'
 
 openapi = TuyaOpenAPI(ENDPOINT, ACCESS_ID, ACCESS_KEY)
 connect = openapi.connect()
-response = openapi.get("/v1.0/iot-03/devices/72286240600194c4d67a/specification")
 
 command_on = {"commands":[{"code":"switch_1","value":True}]}
 command_off = {"commands":[{"code":"switch_1", "value":False}]}
@@ -25,11 +24,11 @@ command_off = {"commands":[{"code":"switch_1", "value":False}]}
 class Tuya_methods:
     @staticmethod
     def send_off():
-        openapi.post("/v1.0/iot-03/devices/72286240600194c4d67a/commands", command_off)
+        openapi.post(f"/v1.0/iot-03/devices/{DEVICE_ID}/commands", command_off)
 
     @staticmethod
     def send_on():
-        openapi.post("/v1.0/iot-03/devices/72286240600194c4d67a/commands", command_on)
+        openapi.post(f"/v1.0/iot-03/devices/{DEVICE_ID}/commands", command_on)
 
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
@@ -105,23 +104,28 @@ def update_schedule():
         if encendido_times[i] and apagado_times[i]:
             schedule.every().day.at(encendido_times[i]).do(lambda: [Tuya_methods.send_on(), log_event('Encendido')])
             schedule.every().day.at(apagado_times[i]).do(lambda: [Tuya_methods.send_off(), log_event('Apagado')])
-        else:
-            print(f"Hora inválida detectada: Encendido = {encendido_times[i]}, Apagado = {apagado_times[i]}")
+
+    # Ejecuta las tareas programadas en un thread separado
+    def run_scheduled_tasks():
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    
+    threading.Thread(target=run_scheduled_tasks).start()
 
 @app.route('/api/run-schedule', methods=['POST'])
 def run_schedule():
     update_schedule()
-
-    while True:
-        schedule.run_pending()
-        time.sleep(10)  # Revisa cada 10 segundos si hay algo que hacer
-
-    return {"status": "success"}, 200
+    return jsonify({"status": "success"}), 200
 
 @app.route('/api/update-schedule', methods=['POST'])
 def update_schedule_endpoint():
     update_schedule()
-    return {"status": "success"}, 200
+    return jsonify({"status": "success"}), 200
+
+# Handler para Vercel
+def handler(request, *args):
+    return app(*args, **request)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
